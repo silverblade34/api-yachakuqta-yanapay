dfc5e823f55846961807d6190671300890d93ece
Michell Pacheco Tacay <mpacheco.tacay@gmail.com>
2024-04-29 10:04:12 -0500

    se estan creando los modulos basicos para la gestion del proceso app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StudentsModule } from './modules/students/students.module';
+import { TeachersModule } from './modules/teachers/teachers.module';
+import { AdministratorsModule } from './modules/administrators/administrators.module';
+import { QuestionsModule } from './modules/questions/questions.module';
+import { AnswersModule } from './modules/answers/answers.module';
+import { ChallengesModule } from './modules/challenges/challenges.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot("mongodb://localhost:27017") ,StudentsModule, TeachersModule, AdministratorsModule, QuestionsModule, AnswersModule, ChallengesModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
